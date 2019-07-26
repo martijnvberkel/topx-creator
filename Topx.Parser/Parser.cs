e48cdb180b58e8fd803859d19b9692e9ffe54357
mberkel <martijnvanberkel@mvbworks.com>
2019-07-26 11:19:20 +0200

    2.0.5.0

--- Topx.Parser/Parser.cs
+++ Topx.Parser/Parser.cs
@@ -227,7 +227,7 @@ namespace Topx.Creator
                 new TopX.relatieType()
                 {
                     relatieID = new TopX.nonEmptyStringTypeAttribuut() {Value = dossier.IdentificatieKenmerk},
-                    typeRelatie = new TopX.nonEmptyStringTypeAttribuut() {Value = dossier.Relatie_Type ?? "Hiërachisch" },
+                    typeRelatie = new TopX.nonEmptyStringTypeAttribuut() {Value = dossier.Relatie_Type ?? "Hiërarchisch" },
                     datumOfPeriode = new datumOfPeriodeType()
                     {
                         datum = relatie_DatumOfPeriode
