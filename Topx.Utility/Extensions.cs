4852b998074c53422d2082a8a9e9086ebb8ef244
mberkel <martijnvanberkel@mvbworks.com>
2019-11-01 11:21:46 +0100

    Development meerdere event-records in cellen, door pipe gescheiden

--- Topx.Utility/Extensions.cs
+++ Topx.Utility/Extensions.cs
@@ -12,5 +12,12 @@ namespace Topx.Utility
         {
             return Source.Replace(" ", "");
         }
+
+        public static bool AllEqual(params int[] values)
+        {
+            if (values == null || values.Length == 0)
+                return true;
+            return values.All(v => v.Equals(values[0]));
+        }
     }
 }
